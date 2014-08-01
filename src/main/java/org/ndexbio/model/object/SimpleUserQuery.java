9a0a7d11c7f0960351a6ed80f875f1559b584744
Ricardo Rivas <ricardo.rivas.preuss12@gmail.com>
2014-08-01 16:13:51 -0700

    added field to SimpleUserQuery

--- src/main/java/org/ndexbio/model/object/SimpleUserQuery.java
+++ src/main/java/org/ndexbio/model/object/SimpleUserQuery.java
@@ -1,5 +1,14 @@
 package org.ndexbio.model.object;
 
 public class SimpleUserQuery extends SimpleQuery{
-
+	
+	private String _accountName;
+	
+	public void setAccountName(String accountName) {
+		_accountName = accountName;
+	}
+	
+	public String getAccountName() {
+		return _accountName;
+	}
 }
