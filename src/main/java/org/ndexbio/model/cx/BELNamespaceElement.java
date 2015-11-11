1356a9f22ac8d03e333ab5fdb25cb4e8fecea3f2
chenjing <jingjingbic@gmail.com>
2015-11-10 16:52:46 -0800

    More on namespace download function.

---
+++ src/main/java/org/ndexbio/model/cx/BELNamespaceElement.java
@@ -0,0 +1,52 @@
+package org.ndexbio.model.cx;
+
+
+import org.cxio.aspects.datamodels.AbstractAspectElement;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class BELNamespaceElement extends AbstractAspectElement {
+
+	final public static String ASPECT_NAME           = "BELNamespaceFiles";
+	
+	private String prefix;
+	private String content; 
+
+	public BELNamespaceElement() {
+	}
+	
+	
+
+	@Override
+	public String getAspectName() {
+		return ASPECT_NAME;
+	}
+
+
+
+	public String getPrefix() {
+		return prefix;
+	}
+
+
+
+	public void setPrefix(String prefix) {
+		this.prefix = prefix;
+	}
+
+
+
+	public String getContent() {
+		return content;
+	}
+
+
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+
+}
