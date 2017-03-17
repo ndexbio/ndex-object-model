21f5f53683125ac709c1199bd853cd3ff3126116
chenjing <jingjingbic@gmail.com>
2017-03-17 12:07:11 -0700

    Added NiceCXNetwork to data model.

---
+++ src/main/java/org/ndexbio/model/cx/NiceCXNetwork.java
@@ -0,0 +1,168 @@
+package org.ndexbio.model.cx;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.cxio.aspects.datamodels.EdgeAttributesElement;
+import org.cxio.aspects.datamodels.EdgesElement;
+import org.cxio.aspects.datamodels.NetworkAttributesElement;
+import org.cxio.aspects.datamodels.NodeAttributesElement;
+import org.cxio.aspects.datamodels.NodesElement;
+import org.cxio.core.interfaces.AspectElement;
+import org.cxio.metadata.MetaDataCollection;
+
+/**
+ * Note: the node and edge attribute tables are indexed on node and edge id respectively. 
+ * However, the po field is still in the compact format ( a list) in the attributeElement object. So don't use that field in this model.
+ * @author chenjing
+ *
+ */
+
+public class NiceCXNetwork {
+
+	private MetaDataCollection metadata;
+	
+	private NamespacesElement namespaces;
+	private Map<Long, NodesElement> nodes;
+	private Map<Long, EdgesElement> edges;
+	private Map<Long, CitationElement> citations;
+	private Map<Long, SupportElement> supports;
+	
+	private Map<Long, Collection<NodeAttributesElement>> nodeAttributes;
+	private Map<Long, Collection<EdgeAttributesElement>> edgeAttributes;
+	
+	private Collection <NetworkAttributesElement> networkAttributes;
+	
+	//function term, node/edge citation and supports might use these 2 tables.
+	private Map<String,Map<Long,Collection<AspectElement>>> nodeAssociatedAspects;
+	private Map<String,Map<Long, Collection<AspectElement>>> edgeAssociatedAspects;
+	
+	private Map<String, Collection<AspectElement>> opaqueAspects;
+	
+	public NiceCXNetwork() {
+		setMetadata(new MetaDataCollection());
+		namespaces = new NamespacesElement();
+		nodes = new HashMap<>();
+		edges = new HashMap<>();
+		citations = new HashMap<>();
+		supports = new HashMap<>();
+		
+		nodeAttributes = new HashMap<> ();
+		edgeAttributes = new HashMap<> ();
+		
+		networkAttributes = new ArrayList<> ();
+		
+		nodeAssociatedAspects = new HashMap<>();
+		edgeAssociatedAspects = new HashMap<>();
+		
+		opaqueAspects = new HashMap<>();
+		
+	}
+	
+	public void addNode(NodesElement node) {
+		nodes.put(node.getId(), node);
+	}
+	
+	public void addEdge(EdgesElement edge) {
+		edges.put(edge.getId(), edge);
+	}
+	
+	public void addNetworkAttribute ( NetworkAttributesElement networkAttribute) {
+		networkAttributes.add(networkAttribute);
+	}
+	
+	public void addNodeAttribute( NodeAttributesElement nodeAttribute) {	
+			addNodeAttribute(nodeAttribute.getPropertyOf(),nodeAttribute);
+	}
+	
+	public void addNodeAttribute( Long i, NodeAttributesElement nodeAttribute) {
+		
+		Collection<NodeAttributesElement> nodeAttrs = nodeAttributes.get(i);
+		if ( nodeAttrs == null) {
+				nodeAttrs = new LinkedList<>();
+				nodeAttributes.put(i,nodeAttrs);
+		}
+		nodeAttrs.add(nodeAttribute);
+	}
+	
+	public void addEdgeAttribute(EdgeAttributesElement edgeAttribute) {
+			addEdgeAttribute(edgeAttribute.getPropertyOf(),edgeAttribute);
+		
+	}
+
+	
+	public void addEdgeAttribute(Long i , EdgeAttributesElement edgeAttribute) {
+			Collection<EdgeAttributesElement> edgeAttrs = edgeAttributes.get(i);
+			if ( edgeAttrs == null) {
+				edgeAttrs = new LinkedList<>();
+				edgeAttributes.put(i, edgeAttrs);
+			}
+			edgeAttrs.add(edgeAttribute);
+	}
+	
+	public void addSupport(SupportElement e) {
+		supports.put(e.getId(), e);
+	}
+	
+	public void addCitation(CitationElement e) {
+		citations.put(e.getId(),e);
+	}
+	
+	public void addOpapqueAspect(AspectElement e) {
+		Collection<AspectElement> aspectElmts = opaqueAspects.get(e.getAspectName());
+		if ( aspectElmts == null) {
+			aspectElmts = new LinkedList<> ();
+			opaqueAspects.put(e.getAspectName(), aspectElmts);
+		}
+		aspectElmts.add(e);
+		
+	}
+	
+	public void addNodeAssociatedAspectElement(Long nodeId, AspectElement elmt) {
+		addAssciatatedAspectElement(nodeAssociatedAspects, nodeId, elmt);
+	}
+
+	public void addEdgeAssociatedAspectElement(Long edgeId, AspectElement elmt) {
+		addAssciatatedAspectElement(edgeAssociatedAspects, edgeId, elmt);	
+	}
+	
+	private static void addAssciatatedAspectElement(Map<String,Map<Long,Collection<AspectElement>>> table, Long id, AspectElement elmt) {
+		Map<Long,Collection<AspectElement>> aspectElements = table.get(elmt.getAspectName());
+		if ( aspectElements == null) {
+			aspectElements = new TreeMap<> ();
+			table.put(elmt.getAspectName(), aspectElements);
+		}
+		Collection<AspectElement> elmts = aspectElements.get(id);
+		
+		if (elmts == null) {
+			elmts = new ArrayList<>();
+			aspectElements.put(id, elmts);
+		}
+		elmts.add(elmt);
+	}
+
+	
+	public MetaDataCollection getMetadata() {
+		return metadata;
+	}
+
+	public void setMetadata(MetaDataCollection metadata) {
+		this.metadata = metadata;
+	}
+	
+	public void addNameSpace(String prefix, String uri) {
+		namespaces.put(prefix, uri);
+	}
+	
+	public void setNamespaces(NamespacesElement ns ) {
+		this.namespaces = ns;
+	}
+	
+	public Map<Long, EdgesElement> getEdges () {
+		return this.edges;
+	}
+}
