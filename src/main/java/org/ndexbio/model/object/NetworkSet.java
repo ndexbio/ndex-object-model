1d43d69899afcb7faff1132af17201fcb557837e
chenjing <jingjingbic@gmail.com>
2017-05-16 13:15:58 -0700

    Add showcase flag in networkset.

--- src/main/java/org/ndexbio/model/object/NetworkSet.java
+++ src/main/java/org/ndexbio/model/object/NetworkSet.java
@@ -1,7 +1,9 @@
 package org.ndexbio.model.object;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -14,10 +16,15 @@ public class NetworkSet extends NdexExternalObject {
 	private String description;
 	private UUID ownerId;
 	private List<UUID> networks;
+	private boolean showcased;
+	
+    private Map<String, Object> properties;
+
 	
 	public NetworkSet () {
 		super();
 		networks = new ArrayList<>(30);
+		properties = new HashMap<>();
 	}
 	
 	public String getName() {
@@ -43,6 +50,22 @@ public class NetworkSet extends NdexExternalObject {
 	}
 	public void setNetworks(List<UUID> networks) {
 		this.networks = networks;
+	}
+
+	public Map<String, Object> getProperties() {
+		return properties;
+	}
+
+	public void setProperties(Map<String, Object> properties) {
+		this.properties = properties;
+	}
+
+	public boolean isShowcased() {
+		return showcased;
+	}
+
+	public void setShowcased(boolean showcased) {
+		this.showcased = showcased;
 	}
 	
 }
