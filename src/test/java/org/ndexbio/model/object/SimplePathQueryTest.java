4b1e19ec7f162b15b274607e14b82bb9ea82f2b9
chenjing <jingjingbic@gmail.com>
2018-03-01 12:38:42 -0800

    Adding unit test for SimplePathQuery.

---
+++ src/test/java/org/ndexbio/model/object/SimplePathQueryTest.java
@@ -0,0 +1,48 @@
+package org.ndexbio.model.object;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class SimplePathQueryTest {
+
+	@Test
+	public void testMembership() throws JsonParseException, JsonMappingException, IOException {
+		
+		ObjectMapper mapper = new ObjectMapper();
+		
+		SimplePathQuery spq = new SimplePathQuery();
+		spq.setSearchString("foo");
+		spq.setSearchDepth(2);
+		spq.setErrorWhenLimitIsOver(false);
+		spq.setEdgeLimit(200);
+		
+		
+        // serialize object
+        String s = mapper.writeValueAsString( spq);
+        
+        System.out.println (s);
+
+        
+        SimplePathQuery m = mapper.readValue(s, SimplePathQuery.class);
+        
+
+		
+        assertEquals(m.getEdgeLimit(), spq.getEdgeLimit());
+        
+        assertEquals(m.getErrorWhenLimitIsOver(), spq.getErrorWhenLimitIsOver());
+
+        assertEquals(m.getSearchDepth(), spq.getSearchDepth());
+
+        assertEquals(m.getSearchString(), spq.getSearchString());
+
+	}
+	
+	
+}
