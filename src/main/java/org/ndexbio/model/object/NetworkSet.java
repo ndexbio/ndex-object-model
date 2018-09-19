f2ffbdb1e0817e11e6bb96d916451729d7c49019
chenjing <jingjingbic@gmail.com>
2018-09-19 13:22:58 -0700

    Adding jackson binding annotation to not writing nulls out in the object.

--- src/main/java/org/ndexbio/model/object/NetworkSet.java
+++ src/main/java/org/ndexbio/model/object/NetworkSet.java
@@ -8,7 +8,12 @@ import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+ 
+
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(Include.NON_NULL) 
 
 public class NetworkSet extends NdexExternalObject {
 
