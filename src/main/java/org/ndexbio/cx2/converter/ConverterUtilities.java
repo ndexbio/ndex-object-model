694086360cf70a9ffcc9be5ef4e403ab61960866
Chris Churas <churas.camera@gmail.com>
2020-09-21 17:14:41 -0700

    updated to always raise NdexException and modified to return ConverterUtilitiesResult object to let caller find out any conversion warnings

--- src/main/java/org/ndexbio/cx2/converter/ConverterUtilities.java
+++ src/main/java/org/ndexbio/cx2/converter/ConverterUtilities.java
@@ -1,44 +1,74 @@
 package org.ndexbio.cx2.converter;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.ndexbio.model.exceptions.NdexException;
 
 public class ConverterUtilities {
-
+    
+        protected static final Pattern passThruPattern = Pattern.compile("^COL=(.*),T=.*$");
 	
-	/** covert a cytoscape string VP value into a CX2 VP object value
-	 * 
-	 * @param cytoscapeDataType
-	 * @param value
-	 * @return
-	 * @throws NdexException
+	/** 
+          * Convert a cytoscape string VP value into a CX2 VP object value using
+          * {@link java.lang.Double#valueOf}, {@link java.lang.Integer#valueOf}, 
+          * {@link java.lang.Long#valueOf}, and {@link java.lang.Boolean#valueOf} to
+          * perform conversion. (Strings are returned as is)
+          * 
+	 * @param cytoscapeDataType datatype to convert to. Should be one of the following
+          *                           string, double, integer, long, boolean
+	 * @param value The value to convert.
+	 * @return The converted result along with any warnings about conversion
+	 * @throws NdexException if either parameter passed in is {@code null}, if there was a
+         *                        conversion error, or if the {@code cytoscapeDataType} is not
+         *                        one of the valid types listed above
 	 */
-	public static Object cvtStringValueToObj(String cytoscapeDataType, String value) throws NdexException {
-		if ( cytoscapeDataType.equals("string")) {
-			return value;
-		} else if (cytoscapeDataType.equals("double")) {
-		   return Double.valueOf(value);
-		}else if (cytoscapeDataType.equals("long")) {
-			try {
-			   return Long.valueOf(value);
-			} catch (NumberFormatException e) {
-				System.err.println("Value " + value + " is not a valid string for long. NDEx is converting it to long from double.");
-				return Long.valueOf(Double.valueOf(value).longValue());		
-			}
-		}else if (cytoscapeDataType.equals("integer")) {
-			try {
-				return Integer.valueOf(value);
-			}  catch (NumberFormatException e) {
-				System.err.println("Value " + value + " is not a valid string for integer. NDEx is converting it to long from double.");
-				return Integer.valueOf(Double.valueOf(value).intValue());		
-			}
-		}else if (cytoscapeDataType.equals("boolean")) {
-				   return Boolean.valueOf(value);
-		} else {
-			throw new NdexException ("Unsupported cy data type found:" + cytoscapeDataType);
+	public static ConverterUtilitiesResult cvtStringValueToObj(final String cytoscapeDataType,
+                final String value) throws NdexException {
+	        if (cytoscapeDataType == null){
+                    throw new NdexException("Unsupported cy data type: <null>");
+                 }
+                 if (value == null){
+                     throw new NdexException("Unsupported cy data value: <null>");
+                 }
+                 
+                 if ( cytoscapeDataType.equals("string")) {
+			return new ConverterUtilitiesResult(value, null);
 		}
+                 try {
+                    if (cytoscapeDataType.equals("double")) {
+
+                      return new ConverterUtilitiesResult(Double.valueOf(value), null);
+                    }
+
+                    if (cytoscapeDataType.equals("long")) {
+                           try {
+                              return new ConverterUtilitiesResult(Long.valueOf(value), null);
+                           } catch (NumberFormatException e) {
+                                      return new ConverterUtilitiesResult(Long.valueOf(Double.valueOf(value).longValue()),
+                                            Arrays.asList("Value " + value
+                                                    + " is not a valid string for long. NDEx is converting it to long from double: " + e.getMessage()));		
+                           }
+                   }
+                    if (cytoscapeDataType.equals("integer")) {
+                           try {
+                                   return new ConverterUtilitiesResult(Integer.valueOf(value), null);
+                           }  catch (NumberFormatException e) {
+                                   return new ConverterUtilitiesResult(Integer.valueOf(Double.valueOf(value).intValue()),
+                                           Arrays.asList("Value " + value
+                                                   + " is not a valid string for integer. NDEx is converting it to integer from double: " + e.getMessage()));		
+                           }
+                   }
+
+                    if (cytoscapeDataType.equals("boolean")) {
+                       return new ConverterUtilitiesResult(Boolean.valueOf(value), null);
+                   } 
+                 } catch(NumberFormatException nfe){
+                     throw new NdexException("Error converting data to type: " + cytoscapeDataType + " : " + nfe.getMessage());
+                 }
+		throw new NdexException ("Unsupported cy data type found: " + cytoscapeDataType);
+		
 	}
 
 	/**
@@ -47,13 +77,16 @@ public class ConverterUtilities {
 	 * @return
 	 * @throws NdexException
 	 */
-    public static String getPassThroughMappingAttribute(String mappingString) throws NdexException {
-		Pattern p = Pattern.compile("^COL=(.*),T=.*$");
-		Matcher m = p.matcher(mappingString);
-		if ( m.matches() ) {
-			return  m.group(1);
-		}
-		throw new NdexException("Malformed mapping string for Passthrough mapping: " + mappingString);
+    public static String getPassThroughMappingAttribute(final String mappingString) throws NdexException {
+            if (mappingString == null){
+                throw new NdexException("Mapping string for Passthrough mapping is null");
+            }
+           Matcher m = ConverterUtilities.passThruPattern.matcher(mappingString);
+           if ( m.matches() ) {
+                   return  m.group(1);
+           }
+           throw new NdexException("Malformed mapping string for Passthrough mapping: "
+                   + mappingString);
     }
 
 
