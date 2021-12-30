ac60e90013f503016f5310ec236aa5e7aa821d13
Chris Churas <churas.camera@gmail.com>
2021-12-29 16:37:37 -0800

    added geneAnnotationServices. Fix for UD-2003

--- src/main/java/org/ndexbio/ndexsearch/rest/model/Query.java
+++ src/main/java/org/ndexbio/ndexsearch/rest/model/Query.java
@@ -1,17 +1,21 @@
 package org.ndexbio.ndexsearch.rest.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Represents a Query
  * @author churas
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Query {
     
     private List<String> _geneList;
     private List<String> _sourceList;
-
+	private Map<String, String> _geneAnnotationServices;
+	
     @Schema(description="List of genes")
     public List<String> getGeneList() {
         return _geneList;
@@ -29,4 +33,13 @@ public class Query {
     public void setSourceList(List<String> _sourceList) {
         this._sourceList = _sourceList;
     }
+	
+	@Schema(description="Map of gene Annotation services")
+	public Map<String, String> getGeneAnnotationServices(){
+		return this._geneAnnotationServices;
+	}
+	
+	public void setGeneAnnotationServices(Map<String, String> _geneAnnotationServices){
+		this._geneAnnotationServices = _geneAnnotationServices;
+	}
 }
