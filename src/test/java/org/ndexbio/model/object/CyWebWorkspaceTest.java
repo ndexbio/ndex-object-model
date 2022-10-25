48dcb64531e8662152450d9a50ac41ce3f0335f3
jingjingbic <jingjingbic@gmail.com>
2022-10-25 10:13:06 -0700

    Adding Cy-web workspace object to model. (CW-5)

---
+++ src/test/java/org/ndexbio/model/object/CyWebWorkspaceTest.java
@@ -0,0 +1,42 @@
+package org.ndexbio.model.object;
+
+import static org.junit.Assert.*;
+
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.UUID;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class CyWebWorkspaceTest {
+
+	@Test
+	public void test() throws JsonProcessingException {
+		ObjectMapper mapper = new ObjectMapper();
+		
+		CyWebWorkspace ws = new CyWebWorkspace();
+		UUID randomId = UUID.randomUUID();
+		ws.setExternalId(randomId);
+		Timestamp current = new Timestamp(Calendar.getInstance().getTimeInMillis());
+		ws.setCreationTime(current);
+		ws.setModificationTime(current);
+		ws.setName("foo");
+		
+		String s = mapper.writeValueAsString( ws);
+		
+		System.out.println(s);
+		assertTrue ( s.contains("\"workspaceId\":"));
+		assertTrue ( !s.contains("externalId"));
+		
+		 CyWebWorkspace ws2= mapper.readValue(s, CyWebWorkspace.class);
+		 
+		 assertEquals (ws2.getExternalId(), ws.getWorkspaceId());
+		 assertEquals ( ws2.getModificationTime(), ws.getModificationTime());
+		 assertEquals (ws2.getCreationTime(), ws.getCreationTime());
+		 assertEquals (ws2.getName(), "foo");
+	}
+
+}
