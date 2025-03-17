e9237d84598d4c750acbef0c483ab290721fc278
dzoanka <joanna.lenkiewicz@gmail.com>
2025-03-17 15:15:25 -0700

    Initial revision

---
+++ src/main/java/org/ndexbio/model/object/Shortcut.java
@@ -0,0 +1,29 @@
+package org.ndexbio.model.object;
+
+import java.util.UUID;
+
+public class Shortcut extends NdexExternalObject {
+	private String name;
+	private UUID parent;
+	private UUID target;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public UUID getParent() {
+		return parent;
+	}
+	public void setParent(UUID parent) {
+		this.parent = parent;
+	}
+	public UUID getTarget() {
+		return target;
+	}
+	public void setTarget(UUID target) {
+		this.target = target;
+	}
+
+}
