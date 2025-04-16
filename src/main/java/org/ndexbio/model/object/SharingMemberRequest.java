65451319f3cbed1796fc603a845542fb4e3e6a82
dzoanka <joanna.lenkiewicz@gmail.com>
2025-04-16 14:45:38 -0700

    Fix type

--- src/main/java/org/ndexbio/model/object/SharingMemberRequest.java
+++ src/main/java/org/ndexbio/model/object/SharingMemberRequest.java
@@ -1,12 +1,11 @@
 package org.ndexbio.model.object;
 
-import java.security.Permission;
 import java.util.Map;
 import java.util.UUID;
 
 public class SharingMemberRequest {
     private Map<UUID, FileType> files;       // map of file UUID and type: "folder" or "network"
-    private Map<UUID,Permission> members;
+    private Map<UUID, Permissions> members;
 
     public SharingMemberRequest() {}
 
@@ -16,10 +15,10 @@ public class SharingMemberRequest {
     public void setFiles(Map<UUID, FileType> files) {
         this.files = files;
     }
-    public Map<UUID,Permission> getMembers() {
+    public Map<UUID,Permissions> getMembers() {
         return members;
     }
-    public void setMembers(Map<UUID,Permission> members) {
+    public void setMembers(Map<UUID,Permissions> members) {
         this.members = members;
     }
 }
