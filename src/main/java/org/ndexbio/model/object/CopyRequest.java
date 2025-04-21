f9d7ebc2610824a7c1da7c362c73dfac8e802898
dzoanka <joanna.lenkiewicz@gmail.com>
2025-04-21 09:27:03 -0700

    Refactor copy request

--- src/main/java/org/ndexbio/model/object/CopyRequest.java
+++ src/main/java/org/ndexbio/model/object/CopyRequest.java
@@ -3,17 +3,17 @@ package org.ndexbio.model.object;
 import java.util.UUID;
 
 public class CopyRequest {
-    private UUID from_uuid;
+    private UUID fileId;
     private FileType type;    // "network" or "shortcut"
-    private UUID to_path;   // Target folder ID
+    private UUID targetId;   // Target folder ID
 
     public CopyRequest() {}
 
-    public UUID getFrom_uuid() {
-        return from_uuid;
+    public UUID getFileId() {
+        return fileId;
     }
-    public void setFrom_uuid(UUID from_uuid) {
-        this.from_uuid = from_uuid;
+    public void setFileId(UUID fileId) {
+        this.fileId = fileId;
     }
 
     public FileType getType() {
@@ -23,10 +23,10 @@ public class CopyRequest {
         this.type = type;
     }
 
-    public UUID getTo_path() {
-        return to_path;
+    public UUID getTargetId() {
+        return targetId;
     }
-    public void setTo_path(UUID to_path) {
-        this.to_path = to_path;
+    public void setTargetId(UUID targetId) {
+        this.targetId = targetId;
     }
 }
