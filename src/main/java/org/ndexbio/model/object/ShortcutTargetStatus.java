fa814514f1a63f44938eec2ca009acef0fdd8106
dzoanka <joanna.lenkiewicz@gmail.com>
2025-05-12 15:49:33 -0700

    Add docs

--- src/main/java/org/ndexbio/model/object/ShortcutTargetStatus.java
+++ src/main/java/org/ndexbio/model/object/ShortcutTargetStatus.java
@@ -1,8 +1,16 @@
 package org.ndexbio.model.object;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Represents the status of a shortcut's target")
 public enum ShortcutTargetStatus {
+	@Schema(description = "Target is active and accessible")
 	ACTIVE,
+	
+	@Schema(description = "Target is in trash but can be restored")
 	IN_TRASH,
+	
+	@Schema(description = "Target has been permanently deleted")
 	DELETED
 }
 
