9a6643cf8b6be6501347c70820b0fa3639559ef3
Chris Churas <churas.camera@gmail.com>
2025-09-05 16:20:30 -0700

    added shortname for toString() call

--- src/main/java/org/ndexbio/model/object/FileType.java
+++ src/main/java/org/ndexbio/model/object/FileType.java
@@ -1,7 +1,19 @@
 package org.ndexbio.model.object;
 
 public enum FileType {
-    NETWORK,
-    FOLDER,
-    SHORTCUT
+    NETWORK("network"),
+    FOLDER("folder"),
+    SHORTCUT("shortcut");
+	
+	private final String shortName; // Field to store the custom string
+	
+	FileType(String shortName){
+		this.shortName = shortName;
+	}
+	
+	// Override toString() to return the custom string
+    @Override
+    public String toString() {
+        return shortName;
+    }
 }
